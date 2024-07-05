c2d96627fde10b01b581371ae0e1518058dd0f1c
William Andry <williamandrysm98@gmail.com>
2024-07-05 01:25:12 -0600

    Proyecto concetado con el API modelo try, then y catch

--- ejemplos/script.js
+++ ejemplos/script.js
@@ -1,3 +1,54 @@
-const url  = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=5"
 
-fetch(url)
+const url  = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=15" 
+
+async function listaImagen(){
+    try{
+        let fetchImagen = await fetch(url)
+        let datosImagenes = await fetchImagen.json()
+
+        console.log(datosImagenes)
+        const card = document.querySelector("[data-ul]") 
+
+       
+            datosImagenes.forEach(elemento => {   
+                const contenido = 
+                `<li class="card">
+                <img class="card__image" src="${elemento.url}" alt="imagen">
+                <h3 class="card__title">${elemento.title}</h3></li>
+                `
+               
+                card.innerHTML = card.innerHTML + contenido
+            })
+    }
+    catch(error){
+        console.log(error)
+    }
+}
+
+listaImagen()
+
+/* function listaImagenes(){   
+    fetch(url)  
+        .then( response => response.json()) 
+        .then( datosImagenes=> {    
+
+    
+            console.log(datosImagenes) 
+
+           
+            const card = document.querySelector("[data-ul]") 
+
+       
+            datosImagenes.forEach(elemento => {   
+                const contenido = `<li class="card">
+                <img class="card__image" src="${elemento.url}" alt="imagen">
+                <h3 class="card__title">${elemento.title}</h3></li>
+                `
+               
+                card.innerHTML = card.innerHTML + contenido
+            })
+        })
+        .catch(error => console.log("Error"))
+}
+
+listaImagenes() */
